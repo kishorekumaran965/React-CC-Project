aa3c7700f7b660dd2e2c53b808aedfb9f77d4df4
kishorekumaran965 <150883648+kishorekumaran965@users.noreply.github.com>
2024-11-13 14:59:50 +0530

    Update UserContext.js

--- UserContext.js
+++ UserContext.js
@@ -1,1 +1,30 @@
 
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password, users) => {
+    const user = users.find((u) => u.email === email && u.password === password);
+    if (user) {
+      setUser(user);
+      return true;
+    } else {
+      return false;--
+      
+    }
+  };
+
+  const signup = (newUser, callback) => {
+    setUser(newUser); 
+    callback();
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, signup }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
